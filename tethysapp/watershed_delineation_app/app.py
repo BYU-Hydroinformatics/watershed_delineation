7b82c3643690e2f3f057d3a3ff28de95dae96236
xhqiao89 <xhqiao89@gmail.com>
2017-12-29 11:58:49 -0700

    first commit

---
+++ tethysapp/watershed_delineation_app/app.py
@@ -0,0 +1,37 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class WatershedDelineationApp(TethysAppBase):
+    """
+    Tethys app class for Watershed Delineation App.
+    """
+
+    name = 'Watershed Delineation App'
+    index = 'watershed_delineation_app:home'
+    icon = 'watershed_delineation_app/images/icon.gif'
+    package = 'watershed_delineation_app'
+    root_url = 'watershed-delineation-app'
+    color = '#27ae60'
+    description = 'Place a brief description of your app here.'
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='watershed-delineation-app',
+                controller='watershed_delineation_app.controllers.home'
+            ),
+            UrlMap(name='run',
+                   url='watershed-delineation-app/run',
+                   controller='watershed_delineation_app.controllers.run_wd')
+        )
+
+        return url_maps
---
+++ tethysapp/watershed_delineation_app/watersheddelineationapp_process.py
@@ -0,0 +1,48 @@
+import os
+import binascii
+from pywps import Process, LiteralInput, ComplexInput, ComplexOutput, Format, FORMATS
+from tethys_apps.tethysapp.watershed_delineation_app.controllers import WD
+
+class watersheddelineationprocess(Process):
+    def __init__(self):
+        # init process
+        inputs = [LiteralInput('outlet_x', 'Outlet Longitude', data_type='float', allowed_values=[[-8010000, 7570000]]),
+                  LiteralInput('outlet_y', 'Outlet Latitude', data_type='float', allowed_values=[[-1923000, 2270000]])]
+        outputs = [ComplexOutput('watershed', 'Delineated Watershed', supported_formats=[Format('application/gml+xml')])]
+
+        super(watersheddelineationprocess, self).__init__(
+            self._handler,
+            identifier='watersheddelineationprocess', # must be same, as filename
+            version='1.0',
+            title="Watershed delineation process",
+            abstract='This process provides watershed delineation function using GRASS within DR country area',
+            profile='',
+            inputs=inputs,
+            outputs=outputs,
+            store_supported=True,
+            status_supported=True
+        )
+
+
+    def _handler(self, request, response):
+
+        #get input values
+        xlon = request.inputs['outlet_x'][0].data
+        ylat = request.inputs['outlet_y'][0].data
+        prj = "native"
+        string_length = 4
+        jobid = binascii.hexlify(os.urandom(string_length))
+
+        # Run SC()
+        basin_GEOJSON, msg = WD(jobid, xlon, ylat, prj)
+
+        print basin_GEOJSON
+
+
+        response.outputs['watershed'].output_format = FORMATS.GML
+        response.outputs['watershed'].file = basin_GEOJSON
+
+        print response.outputs['watershed'].file
+
+        return response
+
